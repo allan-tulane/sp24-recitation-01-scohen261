b80b70cf5a3f217e9d65584da245df944bb4a73e
scohen261 <127058745+scohen261@users.noreply.github.com>
2024-01-31 13:04:24 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,78 +2,125 @@
 CMPS 2200  Recitation 1
 """
 
+### the only imports needed are here
 import tabulate
+
 import time
-
-
 ###
 
-def linear_search(mylist, key):
-    """ done. """
-    for i, v in enumerate(mylist):
-        if v == key:
-            return i
-    return -1
 
-def test_linear_search():
-    """ done. """
-    assert linear_search([1, 2, 3, 4, 5], 5) == 4
-    assert linear_search([1, 2, 3, 4, 5], 1) == 0
-    assert linear_search([1, 2, 3, 4, 5], 6) == -1
+def linear_search(mylist, key):
+  """ done. """
+  for i, v in enumerate(mylist):
+    if v == key:
+      return i
+  return -1
+
 
 def binary_search(mylist, key):
-    """ done. """
-
-    return _binary_search(mylist, key, 0, len(mylist) - 1)
-
+  """ done. """
+  return _binary_search(mylist, key, 0, len(mylist) - 1)
 
 
 def _binary_search(mylist, key, left, right):
-  while left <= right:
-    mid = (left + right) // 2
-    if mylist[mid] == key:
-        return mid
-    elif mylist[mid] > key:
-        right = mid - 1
-    else:
-        left = mid + 1
-  return -1
+  """
+  Recursive implementation of binary search.
 
-def test_binary_search():
-  assert binary_search([1,2,3,4,5], 5) == 4
-  assert binary_search([1,2,3,4,5], 1) == 0
-  assert binary_search([1,2,3,4,5], 6) == -1
-  assert binary_search([5,6,7,8,9], 6) == 1
-  assert binary_search([5,6,7,8,9], 8) == 3
+  Params:
+    mylist....list to search
+    key.......search key
+    left......left index into list to search
+    right.....right index into list to search
+
+  Returns:
+    index of key in mylist, or -1 if not present.
+  """
+  ### TODO
+
+  ###
+  if left > right:
+    return -1
+
+  mid = (left + right) // 2
+
+  if mylist[mid] == key:
+    return mid
+  elif mylist[mid] > key:
+    right = mid - 1
+    return _binary_search(mylist, key, left, right)
+  else:
+    left = mid + 1
+    return _binary_search(mylist, key, left, right)
+
 
 def time_search(search_fn, mylist, key):
-    start = time.time()
-    result = search_fn(mylist, key)
-    end = time.time()
-    return (end - start) * 1000, result  # Return both time and result
+  """
+  Return the number of milliseconds to run this
+  search function on this list.
+
+  Note 1: `search_fn` parameter is a function.
+  Note 2: time.time() returns the current time in seconds. 
+  You'll have to multiple by 1000 to get milliseconds.
+
+  Params:
+    sort_fn.....the search function
+    mylist......the list to search
+    key.........the search key 
+
+  Returns:
+    the number of milliseconds it takes to run this
+    search function on this input.
+  """
+  ### TODO
+  startTime = time.time()
+  search_fn(mylist, key)
+  endTime = time.time()
+  elapsedTime = (endTime - startTime) * 1000
+
+  return elapsedTime
+  ###
 
 
 def compare_search(sizes=[1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7]):
+  """
+  Compare the running time of linear_search and binary_search
+  for input sizes as given. The key for each search should be
+  -1. The list to search for each size contains the numbers from 0 to n-1,
+  sorted in ascending order. 
+
+  You'll use the time_search function to time each call.
+
+  Returns:
+    A list of tuples of the form
+    (n, linear_search_time, binary_search_time)
+    indicating the number of milliseconds it takes
+    for each method to run on each value of n
+  """
+  ### TODO
   results = []
-  for n in sizes:
-    n = int(n)
+
+  for size in sizes:
+    n = int(size)
     mylist = list(range(n))
     key = -1
-    linear_search_time, linear_search_result =       time_search(linear_search, mylist, key)
-  binary_search_time, binary_search_result = time_search(binary_search, mylist, key)
-  results.append((n, linear_search_time, linear_search_result,   binary_search_time, binary_search_result))
+
+    lin_time = time_search(linear_search, mylist, key)
+    bin_time = time_search(binary_search, mylist, key)
+
+    results.append((n, lin_time, bin_time))
+
   return results
- 
 
-
+  ###
 
 
 def print_results(results):
-    """ done """
-    print(tabulate.tabulate(results,
-                            headers=['n', 'linear', 'binary'],
-                            floatfmt=".3f",
-                            tablefmt="github"))
+  """ done """
+  print(
+      tabulate.tabulate(results,
+                        headers=['n', 'linear', 'binary'],
+                        floatfmt=".3f",
+                        tablefmt="github"))
 
- 
+
 print_results(compare_search())
